c9745420573cb4dc08200d984b7d43867647be8d
Aniket <aniketchawardol@gmail.com>
2025-02-03 22:49:48 +0530

    refactor: simplify layout by removing unnecessary margin from App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import CodeEditor from "./components/CodeEditor";
 function App() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center w-full h-lvh bg-gray-800 mx-10 my-10 rounded-lg">
+      <div className="flex flex-col items-center justify-center w-full h-lvh bg-gray-800 rounded-lg">
         <CodeEditor/>
       </div>
     </>
